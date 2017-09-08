fecae6b1876dc4ee1a8e6536420c8b8d69ceee56
Gennaro Anzalone <g.anzalone12@gmail.com>
2017-09-08 13:02:02 -0700

    update output message

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -7,8 +7,8 @@ second_word = [gets.chomp]
 
 for i in 0..first_word.length-1
   if first_word[i].chars.sort == second_word[i].chars.sort
-    puts true
+    puts "this is an anagram"
   else
-    puts false
+    puts "this is not an anagram"
   end
 end
