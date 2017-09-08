2d3739beef61f7328c6e0771c3f8cc2daaab7ed4
Gennaro Anzalone <g.anzalone12@gmail.com>
2017-09-08 16:54:08 -0700

    continue work on sunday

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,3 +1,9 @@
+require('pry')
+
+def intialize(first_word, second_word)
+  @first_word = first_word.downcase
+  @second_word = second_word.downcase
+end
 
 puts "Please enter first word"
 first_word = [gets.chomp]
@@ -5,10 +11,16 @@ first_word = [gets.chomp]
 puts "Please enter second word"
 second_word = [gets.chomp]
 
-for i in 0..first_word.length-1
-  if first_word[i].chars.sort == second_word[i].chars.sort
-    puts "this is an anagram"
-  else
-    puts "this is not an anagram"
+class String
+
+binding.pry
+  def anagram?
+
+    if first_word[i].chars.sort == second_word[i].chars.sort
+      puts "this is an anagram"
+    else
+      puts "this is not an anagram"
+    end
+    binding.pry
   end
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,6 +1,1 @@
-require('pry')
-require('anagrams')
 
-describe('anagrams')do
-  it('will return true if both inputted words share the same letters')
-end
