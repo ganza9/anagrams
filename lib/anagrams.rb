8e31ea9ac90b13a4ae2f878cd5dcc26804215af3
Gennaro Anzalone <g.anzalone12@gmail.com>
2017-09-17 18:13:23 -0700

    finished project

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,26 +1,89 @@
 require('pry')
 
-def intialize(first_word, second_word)
-  @first_word = first_word.downcase
-  @second_word = second_word.downcase
+class Word
+  attr_reader(:first_word, :second_word)
+  def initialize (first_word,second_word)
+    @first_word = first_word.downcase
+    @second_word = second_word.downcase
+    @first_word_array = @first_word.split(' ')
+    @second_word_array = @second_word.split(' ')
+  end
+
+  def antigram?
+    first_array = @first_word_array.join('').gsub(/[^a-zA-Z]/i, '').split('').uniq
+    second_array = @second_word_array.join.gsub(/[^a-zA-Z]/i, '').split('').uniq
+
+    antigram_array = []
+
+    for i in 1..first_array.length do
+      second_array.each do |array_element|
+        if array_element.include?(first_array[i-1].to_s)
+          antigram_array.push(1)
+        else
+          antigram_array.push(0)
+        end
+      end
+    end
+    if antigram_array.reduce(:+) < 1
+      true
+    else
+      false
+    end
+  end
+
+  def word?
+    combined_inputs = @first_word_array +  @second_word_array
+    input_is_not_word=[]
+
+    for i in 1..combined_inputs.length do
+     if  combined_inputs[(i-1)] !~ /[aeiouy]/
+       input_is_not_word.push(1)
+     else
+       input_is_not_word.push(0)
+      end
+    end
+    if input_is_not_word.reduce(:+) > 0
+      false
+    else
+      true
+    end
+  end
+
+  def anagram?
+    first_word_key = @first_word.gsub(/\s+/, "").split('').sort.join('')
+    second_word_key = @second_word.gsub(/\s+/, "").split('').sort.join('')
+    if first_word_key == second_word_key
+      if @first_word.reverse.split(' ').join('') == @second_word_array.join('')
+        "These words are anagrams of each other and form a palindrome."
+      else
+        "These words are anagrams of each other but don't form a palindrome."
+      end
+    else
+       "This is neither a word or palindrome."
+    end
+  end
 end
 
-puts "Please enter first word"
-first_word = [gets.chomp]
 
-puts "Please enter second word"
-second_word = [gets.chomp]
 
-class String
+puts "Please enter your first word"
+first_entry = gets.chomp
+puts "\n"
+puts "Please enter your second word"
+second_entry = gets.chomp
+puts "\n"
+word_entries = Word.new(first_entry,second_entry)
 
-binding.pry
-  def anagram?
+is_word = word_entries.word?()
+is_anagram = word_entries.anagram?()
+is_antigram = word_entries.antigram?()
 
-    if first_word[i].chars.sort == second_word[i].chars.sort
-      puts "this is an anagram"
-    else
-      puts "this is not an anagram"
-    end
-    binding.pry
+if is_word == true
+  if is_antigram == true
+    puts "The pharase you entered is an antigram"
+  else
+  puts is_anagram
   end
+else
+  puts "Please enter a real word(must contain at least one vowell)."
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,1 +1,36 @@
 
+require('rspec')
+require('anagrams')
+require('pry')
+
+describe('#Anagram_checker')  do
+ it("returns true if the two words inputted are anagrams") do
+   compare_words = Word.new("tea", "eat")
+   expect(compare_words.anagram?()).to(eq("These words are anagrams of each other but don't form a palindrome."))
+ end
+
+ it("accounts for entries of different cases") do
+   compare_words = Word.new("Tea", "eAt")
+   expect(compare_words.anagram?()).to(eq("These words are anagrams of each other but don't form a palindrome."))
+ end
+
+ it("checks whether two words are palindromes") do
+   compare_words = Word.new("Tarawa", "A war at")
+   expect(compare_words.anagram?()).to(eq("These words are anagrams of each other and form a palindrome."))
+ end
+
+ it("checks to make sure every inputted word has at least one vowell") do
+ compare_words = Word.new("Tarawa", "A war at")
+ expect(compare_words.word?()).to(eq(true))
+ end
+
+ it("checks if two inputted words are antigrams") do
+  compare_words = Word.new("xqapx", "vuv")
+  expect(compare_words.antigram?()).to(eq(true))
+ end
+
+ it("checks if two inputted phrases are antigrams") do
+  compare_words = Word.new("xqapx ce", "vuv yo")
+  expect(compare_words.antigram?()).to(eq(true))
+ end
+end
